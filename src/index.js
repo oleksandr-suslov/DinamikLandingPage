5d8cfdb020316b708bb8f5f2cc489a1511e54f8a
oleksandr-suslov <76252100+oleksandr-suslov@users.noreply.github.com>
2021-06-13 11:21:16 +0300

    done
    есть две ошибки
    1. напрямую через JS не загружается фон
    2. когда прописываешь дату вручную ошибка

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ const time = document.getElementById('time'),
     greeting = document.getElementById('greeting'),
     name = document.getElementById('name'),
     focus = document.getElementById('focus');
-
+// Options
+const showAmPm = true;
     // Show Time
-    function showTime() {
+function showTime() {
+        // let today = new Date(2019, 06, 10, 20, 33, 30),
         let today = new Date(),
             hour = today.getHours(),
             min = today.getMinutes(),
@@ -18,39 +20,99 @@ const time = document.getElementById('time'),
         hour = hour % 12 || 12;
         
         //Output Time
-        time.innerHTML = `${addZero(hour)}<span>:</span>${addZero(min)}<span>:</span>${addZero(sec)}`;
+        time.innerHTML = `${addZero(hour)}<span>:</span>${addZero(min)}<span>:</span>${addZero(sec)} ${showAmPm ? amPm : ''}`;
 
         setTimeout(showTime, 1000);
     }
 
 // Add Zeros 
-function addZero (t){
-    return (parseInt(t, 10) < 10 ? '0' : "") + t;
+function addZero (n){
+    return (parseInt(n, 10) < 10 ? '0' : '') + n;
 };
 
 // Set Background and Greeting
-function setBgGreet (){
+function setBgGreet() {
+    // let today = new Date(2019, 06, 10, 20, 33, 30),
     let today = new Date(),
         hour = today.getHours();
 
-    if(hour < 12){
+    if (hour < 12) {
         // It's morning
-        document.body.style.backgroundImage = "url('../src/images/morning.jpg')";
-        greeting.textContent ='Good morning';
-       
-    } else if(hour < 18){
+        // document.body.style.backgroundImage = "url('../src/images/morning.jpg')";
+        greeting.textContent = 'Good morning';
+        document.body.style.color = 'blue';
+            if (document.body.classList.contains('night')) {
+            document.body.classList.replace('night', 'morning');
+        } else {
+            document.body.classList.add('morning');
+        };
+    
+    }
+    
+    else if (hour < 18) {
         // It's afternoon
-        document.body.style.backgroundImage = "url('../src/images/afternoon.jpg')";
-          greeting.textContent ='Good afternoon';
+        // document.body.style.backgroundImage = "url('../src/images/afternoon.jpg')";
+        greeting.textContent = 'Good afternoon';
+        document.body.classList.replace('morning', 'afternoon');
     } else {
         // It's evening
-        document.body.style.backgroundImage = "url('../src/images/night.jpg')";
+        // document.body.style.backgroundImage = "url('../src/images/night.jpg')";
+ document.body.classList.replace('afternoon', 'night');
         greeting.textContent ='Good evening!';
-        // document.body.style.color = 'white';
+        document.body.style.color = 'white';
     }
 };
 
-//Run
+// Get Name
+function getName() {
+    if (localStorage.getItem('name') === null) {
+        name.textContent = '[Enter Name]';
+    } else {
+        name.textContent = localStorage.getItem('name');
+    }
+}
 
+// Set Name
+function setName(e) {
+    if (e.type === 'keypress') {
+// Make sure enter is pressed
+        if (e.wich == 13 || e.keyCode == 13) {
+            localStorage.setItem('name', e.target.innerText);
+            name.blur();
+        }
+    } else {
+            localStorage.setItem('name', e.target.innerText);
+        }
+}
+
+// Get Focus
+function getFocus() {
+    if (localStorage.getItem('focus') === null) {
+        focus.textContent = '[Enter Focus]';
+    } else {
+        focus.textContent = localStorage.getItem('focus');
+    }
+}
+
+// Set Focus
+function setFocus(e) {
+    if (e.type === 'keypress') {
+// Make sure enter is pressed
+        if (e.wich == 13 || e.keyCode == 13) {
+            localStorage.setItem('focus', e.target.innerText);
+            focus.blur();
+        }
+    } else {
+            localStorage.setItem('focus', e.target.innerText);
+        }
+}
+
+name.addEventListener('keypress', setName);
+name.addEventListener('blur', setName);
+focus.addEventListener('keypress', setFocus);
+focus.addEventListener('blur', setFocus);
+//Run
 showTime();
 setBgGreet();
+getName();
+getFocus();
